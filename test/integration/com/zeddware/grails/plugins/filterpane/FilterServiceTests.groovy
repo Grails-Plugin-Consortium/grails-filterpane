858a3b632f4bc6bf1cb3ca3b0dd29829ddf9f41b
Steve Krenek <zeddmaxim@gmail.com>
2009-09-10 04:40:38 +0000

    Tests work now, plus added a few more.

--- test/integration/com/zeddware/grails/plugins/filterpane/FilterServiceTests.groovy
+++ test/integration/com/zeddware/grails/plugins/filterpane/FilterServiceTests.groovy
@@ -1,29 +1,40 @@
 package com.zeddware.grails.plugins.filterpane
 
 import grails.test.*
-import grails.
 
 class FilterServiceTests extends GrailsUnitTestCase {
 
     def filterService
+    def params
 
     protected void setUp() {
         super.setUp()
+        params = ['filter':[ op:[ title:'ILike' ], title:'think' ]]
 
     }
 
     protected void tearDown() {
         super.tearDown()
+        params.clear()
     }
 
     /**
      *
      */
     void testFilterBooksNoCount() {
-        def params = ['filter.op.title':'ILike', 'filter.title':'think']
-
         def results = filterService.filter(params, Book.class, false)
 
-        assertTrue('filter no count failed. ', 1, results?.size())
+        assertEquals('filter no count failed. ', 2, results?.size())
+    }
+
+    void testFilterCount() {
+        assertEquals('filter count failed. ', 2, filterService.count(params, Book.class))
+    }
+
+    void testIdFilter() {
+        params.filter.op.clear()
+        params.filter.op.id = 'Equal'
+        params.filter.id = 4
+        assertEquals('filter id failed. ', 4, filterService.filter(params, Book.class, false)[0].id)
     }
 }
