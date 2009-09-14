87ebf61e095587abbad228ebed57f8fbdf640aad
Steve Krenek <zeddmaxim@gmail.com>
2009-09-14 04:05:28 +0000

    Added 2 more tests.

--- test/integration/com/zeddware/grails/plugins/filterpane/FilterServiceTests.groovy
+++ test/integration/com/zeddware/grails/plugins/filterpane/FilterServiceTests.groovy
@@ -18,9 +18,6 @@ class FilterServiceTests extends GrailsUnitTestCase {
         params.clear()
     }
 
-    /**
-     *
-     */
     void testFilterBooksNoCount() {
         def results = filterService.filter(params, Book.class, false)
 
@@ -36,5 +33,19 @@ class FilterServiceTests extends GrailsUnitTestCase {
         params.filter.op.id = 'Equal'
         params.filter.id = 4
         assertEquals('filter id failed. ', 4, filterService.filter(params, Book.class, false)[0].id)
+    }
+
+    void testAssociatedCollectionFilter() {
+        params = ['filter':[ op:[ 'books.title':'ILike' ], 'books.title':'think' ]]
+        params.filter.op.books = [title:'ILike']
+        params.filter.books = [title:'think']
+        assertEquals('collection filter failed.', 2, filterService.count(params, Author.class))
+    }
+
+    void testAssociationFilter() {
+        params = ['filter':[ op:[ 'author.lastName':'Equal' ], 'author.lastName':'Lewis' ]]
+        params.filter.op.author = [lastName:'Equal']
+        params.filter.author = [lastName:'Lewis']
+        assertEquals('association filter failed.', 'Clive', filterService.filter(params, Book.class)[0].author.firstName)
     }
 }
