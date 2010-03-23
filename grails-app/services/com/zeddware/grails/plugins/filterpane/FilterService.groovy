c2f760b18f907f93a1e78bcf3881d79e43908b1b
Gavin Kromhout <gavin@kromhouts.net>
2010-03-23 06:34:39 +0000

    Format to 4 spaces no tabs, no code changed.

--- grails-app/services/com/zeddware/grails/plugins/filterpane/FilterService.groovy
+++ grails-app/services/com/zeddware/grails/plugins/filterpane/FilterService.groovy
@@ -4,25 +4,25 @@ class FilterService {
 
     boolean transactional = true
     def grailsApplication
-    
+
     def filter(def params, Class filterClass) {
         return filter(params, filterClass, false)
     }
-    
+
     def count(def params, Class filterClass) {
         return filter(params, filterClass, true)
     }
 
     private def filter(def params, Class filterClass, boolean doCount) {
         if (log.isDebugEnabled()) log.debug("filtering... params = ${params.toMapString()}")
-    	//def filterProperties = params?.filterProperties?.tokenize(',')
+        //def filterProperties = params?.filterProperties?.tokenize(',')
         def filterParams = params.filter ? params.filter : params
         def filterOpParams = filterParams.op
         def associationList = []
         def domainClass = FilterUtils.resolveDomainClass(grailsApplication, filterClass)
 
         //if (filterProperties != null) {
-		if (filterOpParams != null && filterOpParams.size() > 0) {
+        if (filterOpParams != null && filterOpParams.size() > 0) {
 
             def c = filterClass.createCriteria()
 
@@ -35,16 +35,16 @@ class FilterService {
                     keyList = keyList.sort() // Sort them to get nested properties next to each other.
 
                     if (log.isDebugEnabled()) log.debug("op Keys = ${keyList}")
-    				
+
                     // op = map entry.  op.key = property name.  op.value = operator.
                     // params[op.key] is the value
                     keyList.each() { propName ->
-                    	if (log.isDebugEnabled()) log.debug("\n=============================================================================.")
-                    	if (log.isDebugEnabled()) log.debug("== ${propName}")
+                        if (log.isDebugEnabled()) log.debug("\n=============================================================================.")
+                        if (log.isDebugEnabled()) log.debug("== ${propName}")
 
                         // Skip associated property entries.  (They'll have a dot in them.)  We'll use the map instead later.
                         if (! propName.contains(".")) {
-                            
+
                             def filterOp = filterOpParams[propName]
                             def rawValue = filterParams[propName]
                             def rawValue2 = filterParams["${propName}To"]
@@ -60,13 +60,13 @@ class FilterService {
                                     c."${propName}"() {
 
                                         filterOp.each() { opEntry ->
-											def associatedDomainProp = FilterUtils.resolveDomainProperty(grailsApplication, domainClass, propName)
+                                            def associatedDomainProp = FilterUtils.resolveDomainProperty(grailsApplication, domainClass, propName)
                                             def associatedDomainClass = associatedDomainProp.referencedDomainClass
                                             def realPropName = opEntry.key
                                             def realOp = opEntry.value
                                             def realRawValue = rawValue[realPropName]
                                             def realRawValue2 = rawValue2 != null ? rawValue2["${realPropName}To"] : null
-											def parsingName = "${propName}.${realPropName}"
+                                            def parsingName = "${propName}.${realPropName}"
                                             def thisDomainProp = FilterUtils.resolveDomainProperty(grailsApplication, associatedDomainClass, realPropName)
 //                                            log.debug("real prop name is ${realPropName}")
                                             def val  = this.parseValue(thisDomainProp, realRawValue, filterParams, parsingName)
@@ -93,10 +93,10 @@ class FilterService {
                                 this.addCriterion(c, propName, filterOp, val, val2, filterParams, thisDomainProp)
                             }
                         }
-                    	if (log.isDebugEnabled()) log.debug("==============================================================================='\n")
+                        if (log.isDebugEnabled()) log.debug("==============================================================================='\n")
                     } // end each op
                 } // end and
-                
+
                 if (doCount) {
                     c.projections {
                         rowCount()
@@ -122,9 +122,9 @@ class FilterService {
 //                    } else if (filterClass.hasProperty('mapping') && filterClass.mapping.hasProperty('mapping') && filterClass.mapping.mapping.hasProperty('sort')) {
 //						if (log.debugEnabled) log.debug('No sort specified and default is specified on domain.  Using it.')
 //						order(domainClass.mapping.sort, params.order ?: 'asc')
-					} else {
-						if (log.debugEnabled) log.debug('No sort parameter or default sort specified.')
-					}
+                    } else {
+                        if (log.debugEnabled) log.debug('No sort parameter or default sort specified.')
+                    }
                 }
             } // end criteria
             def results = null
@@ -132,101 +132,101 @@ class FilterService {
                 results = c.get(criteriaClosure)
             } else {
                 results = c.list(criteriaClosure)
-              
+
             }
             if (doCount && results instanceof List) {
                 //println "Returning count of 0"
                 results = 0I
             }
             return results
-    	} else {
+        } else {
             if (doCount) {
                 return filterClass.count()//0I
             }
             return filterClass.list(params)
-    	}
+        }
     }
-    
+
     private def addCriterion(def criteria, def propertyName, def op, def value, def value2, def filterParams, def domainProperty) {
-    	if (log.isDebugEnabled()) log.debug("Adding ${propertyName} ${op} ${value} value2 ${value2}")
-		boolean added = true
+        if (log.isDebugEnabled()) log.debug("Adding ${propertyName} ${op} ${value} value2 ${value2}")
+        boolean added = true
 
-		// GRAILSPLUGINS-1320.  If value is instance of Date and op is Equal and
-		// precision on date picker was 'day', turn this into a between from
-		// midnight to 1 ms before midnight of the next day.
-		boolean isDayPrecision = "y".equals(filterParams["${domainProperty.domainClass.name}.${domainProperty.name}_isDayPrecision"])
-		boolean isOpAlterable  = (op == 'Equal' || op == 'NotEqual')
-		if (value != null && isDayPrecision == true && Date.isAssignableFrom(value.class) && isOpAlterable) {
-			op = (op == 'Equal') ? 'Between' : 'NotBetween'
-			value = FilterUtils.getBeginningOfDay(value)
-			value2 = FilterUtils.getEndOfDay(value)
-			if (log.isDebugEnabled())
-				log.debug("Date criterion is Equal to day precision.  Changing it to between ${value} and ${value2}")
-		}
+        // GRAILSPLUGINS-1320.  If value is instance of Date and op is Equal and
+        // precision on date picker was 'day', turn this into a between from
+        // midnight to 1 ms before midnight of the next day.
+        boolean isDayPrecision = "y".equals(filterParams["${domainProperty.domainClass.name}.${domainProperty.name}_isDayPrecision"])
+        boolean isOpAlterable  = (op == 'Equal' || op == 'NotEqual')
+        if (value != null && isDayPrecision == true && Date.isAssignableFrom(value.class) && isOpAlterable) {
+            op = (op == 'Equal') ? 'Between' : 'NotBetween'
+            value = FilterUtils.getBeginningOfDay(value)
+            value2 = FilterUtils.getEndOfDay(value)
+            if (log.isDebugEnabled())
+                log.debug("Date criterion is Equal to day precision.  Changing it to between ${value} and ${value2}")
+        }
 
-		if (value) {
-			switch(op) {
-				case 'Equal':
-				criteria.eq(propertyName, value)
-				break
-				case 'NotEqual':
-				criteria.ne(propertyName, value)
-				break
-				case 'LessThan':
-				criteria.lt(propertyName, value)
-				break
-				case 'LessThanEquals':
-				criteria.le(propertyName, value)
-				break
-				case 'GreaterThan':
-				criteria.gt(propertyName, value)
-				break
-				case 'GreaterThanEquals':
-				criteria.ge(propertyName, value)
-				break
-				case 'Like':
-				if (!value.startsWith('*')) value = "*${value}"
-				if (!value.endsWith('*')) value = "${value}*"
-				criteria.like(propertyName, value?.replaceAll("\\*", "%"))
-				break
-				case 'ILike':
-				if (!value.startsWith('*')) value = "*${value}"
-				if (!value.endsWith('*')) value = "${value}*"
-				criteria.ilike(propertyName, value?.replaceAll("\\*", "%"))
-				break
-				case 'NotLike':
-            	if (!value.startsWith('*')) value = "*${value}"
-            	if (!value.endsWith('*')) value = "${value}*"
-            	criteria.not {
-            		criteria.like(propertyName, value?.replaceAll("\\*", "%"))
-            	}
-            	break
-				case 'NotILike':
-            	if (!value.startsWith('*')) value = "*${value}"
+        if (value) {
+            switch(op) {
+                case 'Equal':
+                criteria.eq(propertyName, value)
+                break
+                case 'NotEqual':
+                criteria.ne(propertyName, value)
+                break
+                case 'LessThan':
+                criteria.lt(propertyName, value)
+                break
+                case 'LessThanEquals':
+                criteria.le(propertyName, value)
+                break
+                case 'GreaterThan':
+                criteria.gt(propertyName, value)
+                break
+                case 'GreaterThanEquals':
+                criteria.ge(propertyName, value)
+                break
+                case 'Like':
+                if (!value.startsWith('*')) value = "*${value}"
                 if (!value.endsWith('*')) value = "${value}*"
-            	criteria.not {
-            		criteria.ilike(propertyName, value?.replaceAll("\\*", "%"))
-            	}
-            	break
-				case 'IsNull':
-				criteria.isNull(propertyName)
-				break
-				case 'IsNotNull':
-				criteria.isNotNull(propertyName)
-				break
-				case 'Between':
-				criteria.between(propertyName, value, value2)
-				break
-				case 'NotBetween':
-				criteria.not { between(propertyName, value, value2) }
-				break
-				default:
-				break
-			} // end op switch
-		} else {  // value is null
-			added = false
-		}
-    	//println "== addCriterion OUT =="
+                criteria.like(propertyName, value?.replaceAll("\\*", "%"))
+                break
+                case 'ILike':
+                if (!value.startsWith('*')) value = "*${value}"
+                if (!value.endsWith('*')) value = "${value}*"
+                criteria.ilike(propertyName, value?.replaceAll("\\*", "%"))
+                break
+                case 'NotLike':
+                if (!value.startsWith('*')) value = "*${value}"
+                if (!value.endsWith('*')) value = "${value}*"
+                criteria.not {
+                    criteria.like(propertyName, value?.replaceAll("\\*", "%"))
+                }
+                break
+                case 'NotILike':
+                if (!value.startsWith('*')) value = "*${value}"
+                if (!value.endsWith('*')) value = "${value}*"
+                criteria.not {
+                    criteria.ilike(propertyName, value?.replaceAll("\\*", "%"))
+                }
+                break
+                case 'IsNull':
+                criteria.isNull(propertyName)
+                break
+                case 'IsNotNull':
+                criteria.isNotNull(propertyName)
+                break
+                case 'Between':
+                criteria.between(propertyName, value, value2)
+                break
+                case 'NotBetween':
+                criteria.not { between(propertyName, value, value2) }
+                break
+                default:
+                break
+            } // end op switch
+        } else {  // value is null
+            added = false
+        }
+        //println "== addCriterion OUT =="
     }
 
     /**
