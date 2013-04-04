0161cb2518bc039c5c8189f76caf3e2589cb569d
ctoestreich <acetrike@yahoo.com>
2013-04-04 12:03:51 -0500

    Adding codenarc and code coverage plugins, git ignore and eclipse files

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,10 +22,57 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+
+
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+        test("org.spockframework:spock-grails-support:0.7-groovy-2.0") {
+            export = false
+        }
+
+    }
+
+    plugins {
+
+        runtime(":hibernate:${grailsVersion}") {
+            export = false
+        }
+
+        runtime(":tomcat:${grailsVersion}") {
+            export = false
+        }
+
+        test(":spock:0.7") {
+            export = false
+        }
+
+        test(":code-coverage:1.2.6") {
+            export = false
+        }
+
+        test(":codenarc:0.17") {
+            export = false
+        }
+    }
+}
+
+coverage {
+    xml = true
+    exclusions = ['**/*Tests*']
+}
+
+codenarc {
+    processTestUnit = false
+    processTestIntegration = false
+    processServices = false
+    processDomain = false
+    propertiesFile = 'codenarc.properties'
+    ruleSetFiles = 'file:grails-app/conf/codenarc.groovy'
+    reports = {
+        CxfClientReport('xml') {                    // The report name 'MyXmlReport' is user-defined; Report type is 'xml'
+            outputFile = 'target/codenarc.xml'      // Set the 'outputFile' property of the (XML) Report
+            title = 'Grails FilterPane Plugin'             // Set the 'title' property of the (XML) Report
+        }
     }
 }
