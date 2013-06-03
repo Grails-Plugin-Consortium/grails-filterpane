4777106d9efc93685bb0018b8ca7ace24e18cdfd
ctoestreich <acetrike@yahoo.com>
2013-06-03 08:58:17 -0500

    Changing install script to not override existing files

--- FilterpaneGrailsPlugin.groovy
+++ FilterpaneGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class FilterpaneGrailsPlugin {
-    def version = "2.1.7"
+    def version = "2.1.8"
     def grailsVersion = "1.3 > *"
 
     def author = "Grails Plugin Consortium"
--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -14,13 +14,15 @@ def copy = {String source, String target ->
 
     // only if dir already exists in, ask to overwrite it
     if(new File(target).exists()) {
-        if(isInteractive && !overwrite && !confirmNone){
-            println 'y = yes, n = no, a = overwrite all, s = skip all'
-            input = grailsConsole.userInput("Overwrite existing files in ${target}?", ["y", "n", "a" ,"s"] as String[])
-        }
-        if(!isInteractive || input == "y" || input == "a") overwrite = true
-        if(input == "a") confirmAll = true
-        if(input == "s") confirmNone = true
+        printMessage "Files arealy exist in ${target}.  If you wish to override, please copy those you wish to override manually from the plugin source."
+        overwrite = false
+//        if(isInteractive && !overwrite && !confirmNone){
+//            println 'y = yes, n = no, a = overwrite all, s = skip all'
+//            input = grailsConsole.userInput("Overwrite existing files in ${target}?", ["y", "n", "a" ,"s"] as String[])
+//        }
+//        if(!isInteractive || input == "y" || input == "a") overwrite = true
+//        if(input == "a") confirmAll = true
+//        if(input == "s") confirmNone = true
     } else {
         ant.mkdir(dir: target)
         overwrite = true    // nothing to overwrite but will be copied (state this in the event message)
@@ -29,7 +31,7 @@ def copy = {String source, String target ->
     if(new File(source).isDirectory()) ant.copy(todir: "$target", overwrite: overwrite) { fileset dir: "$source" }
     else ant.copy(todir: "$target", overwrite: overwrite) { fileset file: "$source" }
 
-    printMessage "Filterpane Files ${overwrite ? '' : 'not '}installed."
+    finished "Filterpane Files ${overwrite ? '' : 'not '}installed."
 }
 
 copy("${pluginBasedir}/grails-app/views/filterpane", "${basedir}/grails-app/views/filterpane")
