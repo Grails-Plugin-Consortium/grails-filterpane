12c796bd5948cccff9016b4e1a3b3e1fc8393f2d
Eric Rodriguez <thewavyx@gmail.com>
2013-08-23 16:03:14 +0200

    Revert "Fix for older groovy version"
    This reverts commit 5213f9ad0b3b1e95f29afaad65f4d4ad6f7a12d8.

--- grails-app/domain/org/grails/plugin/filterpane/nested/Function.groovy
+++ grails-app/domain/org/grails/plugin/filterpane/nested/Function.groovy
@@ -2,8 +2,7 @@ package org.grails.plugin.filterpane.nested
 
 import groovy.transform.ToString
 
-// Temporary fix due to groovy version
-@ToString(includes='name')
+@ToString(includes=['name'])
 class Function {
 
     String name = 'function'
