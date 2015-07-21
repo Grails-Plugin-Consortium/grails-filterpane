843205f577e7a300340d6bf7f4049a54a180fe6a
mansiarora <mansi@intelligrape.com>
2015-07-21 17:10:44 +0530

    Updated BookControllerSpec;

--- src/integration-test/groovy/org/grails/plugin/filterpane/BookControllerSpec.groovy
+++ src/integration-test/groovy/org/grails/plugin/filterpane/BookControllerSpec.groovy
@@ -1,5 +1,6 @@
 package org.grails.plugin.filterpane
 
+import grails.core.GrailsApplication
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import grails.test.mixin.integration.Integration
@@ -8,50 +9,61 @@ import org.springframework.beans.factory.annotation.Autowired
 import spock.lang.Specification
 
 @Integration
-@Mock([Publisher, Author, Book])
 @TestFor(BookController)
 class BookControllerSpec extends Specification {
     @Autowired
     FilterPaneService filterPaneService
+    @Autowired
+    GrailsApplication grailsApplication
 
-    def setup(){
+    def setup() {
         filterPaneService = new FilterPaneService()
-        filterPaneService.grailsApplication= Holders.getGrailsApplication()
+        filterPaneService.grailsApplication = grailsApplication
         controller.filterPaneService = filterPaneService
     }
 
     def "test emdash filter on controller"() {
         given:
-        Publisher p = new Publisher(firstName: 'Some', lastName: 'Publisher').save(validate:false)
-        Author dm = new Author(firstName: 'Dave', lastName: 'Mark', favoriteGenre: FavoriteGenre.Reference, publisher: p).save(validate:false)
-        new Book(authors: [dm], title: 'Hello�how are you', releaseDate: java.sql.Date.valueOf('2008-11-01'), inStock: true, price: 39.99, cost: 27.99, readPriority: 'Normal', bookType: BookType.Reference).save(validate:false)
+        Book.withNewSession {
+            Publisher p = new Publisher(firstName: 'Some', lastName: 'Publisher').save(flush: true, failOnError: true)
+            Author dm = new Author(firstName: 'Dave', lastName: 'Mark', favoriteGenre: FavoriteGenre.Reference, publisher: p).save(flush: true, failOnError: true)
+            new Book(authors: [dm], title: 'Hello�how are you', releaseDate: java.sql.Date.valueOf('2008-11-01'), inStock: true, price: 39.99, cost: 27.99, readPriority: 'Normal', bookType: BookType.Reference).save(flush: true, failOnError: true)
+        }
 
         when:
+        def model
         params.filter = [op: [title: 'ILike'], title: '�']
-        controller.filter()
-        def model = controller.modelAndView.model
+        Book.withNewSession {
+            controller.filter()
+            model = controller.modelAndView.model
+        }
 
         then:
         model.bookList.size() == 1
         model.bookCount == 1
-        model.bookList.find { it.title == 'Hello�how are you'}
+        model.bookList.find { it.title == 'Hello�how are you' }
     }
 
     def "test filter by title text on controller"() {
         given:
-        Publisher p = new Publisher(firstName: 'Some', lastName: 'Publisher').save(validate:false)
-        Author dm = new Author(firstName: 'Dave', lastName: 'Mark', favoriteGenre: FavoriteGenre.Reference, publisher: p).save(validate:false)
-        new Book(authors: [dm], title: 'I like cheese', releaseDate: java.sql.Date.valueOf('2008-11-01'), inStock: true, price: 39.99, cost: 27.99, readPriority: 'Normal', bookType: BookType.Reference).save(validate:false)
-        new Book(authors: [dm], title: 'I like apples', releaseDate: java.sql.Date.valueOf('2008-11-01'), inStock: true, price: 39.99, cost: 27.99, readPriority: 'Normal', bookType: BookType.Reference).save(validate:false)
+        Book.withNewSession {
+            Publisher p = new Publisher(firstName: 'Some', lastName: 'Publisher').save(flush: true, failOnError: true)
+            Author dm = new Author(firstName: 'Dave', lastName: 'Mark', favoriteGenre: FavoriteGenre.Reference, publisher: p).save(flush: true, failOnError: true)
+            new Book(authors: [dm], title: 'I like cheese', releaseDate: java.sql.Date.valueOf('2008-11-01'), inStock: true, price: 39.99, cost: 27.99, readPriority: 'Normal', bookType: BookType.Reference).save(flush: true, failOnError: true)
+            new Book(authors: [dm], title: 'I like apples', releaseDate: java.sql.Date.valueOf('2008-11-01'), inStock: true, price: 39.99, cost: 27.99, readPriority: 'Normal', bookType: BookType.Reference).save(flush: true, failOnError: true)
+        }
 
         when:
+        def model
         params.filter = [op: [title: 'ILike'], title: 'like']
-        controller.filter()
-        def model = controller.modelAndView.model
+        Book.withNewSession {
+            controller.filter()
+            model = controller.modelAndView.model
+        }
 
         then:
         model.bookList.size() == 2
         model.bookCount == 2
-        model.bookList.find { it.title == 'I like cheese'}
+        model.bookList.find { it.title == 'I like cheese' }
     }
 }
